1f539ec129ee85abd543fc5bf0b227690dbec673
jinsungit <jinanderson@gmail.com>
2015-09-29 11:03:45 -0400

    update traffic simulation

--- ps1/traffic.py
+++ ps1/traffic.py
@@ -37,33 +37,38 @@ vmax = 5
 
 
 # road init condition
-R[20] = 4
+R[30] = 4
+R[12] = 3
+R[45] = 5
+R[67] = 3
 R[80] = 5
 R[99] = 5
 
 # random param
-p = 0.5
+p = 0.1
 
 maxIter = 20
 
 for iter in range(0, maxIter):
     printRoad(R)
+    nextR = list(R)
     for i in range(0, numR):
         # for each car
         if R[i]>0:
             dis2nextCar_val = dis2nextCar(R,i)
             # Acceleration
             if R[i]<vmax and dis2nextCar_val>R[i]+1:
-                R[i] = R[i]+1
+                nextR[i] = R[i]+1
             # Slowing down
             if dis2nextCar_val <= R[i]:
-                R[i] = dis2nextCar_val - 1
+                nextR[i] = dis2nextCar_val - 1
             # Randomization
             if random.random() <= p:
-                R[i] = R[i] - 1
-                if R[i] == -1:
-                    R[i] = 0
+                nextR[i] = R[i] - 1
+                if nextR[i] == -1:
+                    nextR[i] = 0
             # Car motion
-            R[(i+R[i]) % numR] = R[i]
-            R[i] = 0
+            nextR[(i+nextR[i]) % numR] = nextR[i]
+            nextR[i] = 0
+    R = nextR
 
