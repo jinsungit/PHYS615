888eca1f33f197069ef48780bb64ed1461f70be9
jinsungit <jinanderson@gmail.com>
2015-10-02 15:05:08 -0400

    update traffic simulation

--- ps1/traffic.py
+++ ps1/traffic.py
@@ -9,16 +9,23 @@ import random
 
 
 # find distance to next car
-# R is road array, i is current car position
-def dis2nextCar(R,i):
-    numR = len(R)
-    for j in range(1,numR):
-        if R[(i + j)%numR] >0:
-            return j
-    return numR 
+# cars is car array, i is current car position
+def dis2nextCar(cars,i):
+    minDist = numR
+    for car in cars:
+        dist = numR
+        if car.position < i: # car is behind
+            dist = (numR-1-i) + (car.position+1)
+            if dist <= minDist:
+                minDist = dist
+    return minDist
 
 # print road
-def printRoad(R):
+def printRoad(cars):
+    R = [0]*numR
+    for car in cars:
+        R[car.position] = car.speed
+
     s = ""
     for r in R:
         if r == 0:
@@ -27,48 +34,62 @@ def printRoad(R):
             s = s + str(r)
     print s
 
+######
+class Car:
+    def __init__(self, speed, position, numR):
+        self.speed    = speed
+        self.position = position
+        self.dis2nextCar_val = 0
+        self.numR = numR
+        self.vmax = 5 # max speed
+    
+    def carActionRules(self):
+        self.dis2nextCar_val = dis2nextCar(cars,self.position)
+        # Acceleration
+        if self.speed < self.vmax and self.dis2nextCar_val > self.speed+1:
+            self.speed = self.speed + 1
+        # Slowing down
+        if self.dis2nextCar_val <= self.speed:
+            self.speed = self.dis2nextCar_val - 1
+        # Randomization
+        if random.random() <= p:
+            # should not have negative speed
+            if self.speed >0:
+                self.speed = self.speed - 1
+        # Car motion
+        self.position = (self.position + self.speed) % self.numR
 
 # road blocks
 numR = 100
-R = [0] * numR
 
-vmax = 5
-
-
+# cars
+numC = 9
 
 # road init condition
-R[30] = 4
-R[12] = 3
-R[45] = 5
-R[67] = 3
-R[80] = 5
-R[99] = 5
+cars = [Car]*numC
+#cars[0] = Car(4,30,numR)
+#cars[1] = Car(5,80,numR)
+#cars[2] = Car(5,99,numR)
+
+cars[0] = Car(4,10,numR)
+cars[1] = Car(5,20,numR)
+cars[2] = Car(5,26,numR)
+cars[3] = Car(5,30,numR)
+cars[4] = Car(0,50,numR)
+cars[5] = Car(0,51,numR)
+cars[6] = Car(1,53,numR)
+cars[7] = Car(4,70,numR)
+cars[8] = Car(5,99,numR)
+
+
+
 
 # random param
-p = 0.1
-
+p = 0.6
 maxIter = 20
 
 for iter in range(0, maxIter):
-    printRoad(R)
-    nextR = list(R)
-    for i in range(0, numR):
+    for car in cars:
         # for each car
-        if R[i]>0:
-            dis2nextCar_val = dis2nextCar(R,i)
-            # Acceleration
-            if R[i]<vmax and dis2nextCar_val>R[i]+1:
-                nextR[i] = R[i]+1
-            # Slowing down
-            if dis2nextCar_val <= R[i]:
-                nextR[i] = dis2nextCar_val - 1
-            # Randomization
-            if random.random() <= p:
-                nextR[i] = R[i] - 1
-                if nextR[i] == -1:
-                    nextR[i] = 0
-            # Car motion
-            nextR[(i+nextR[i]) % numR] = nextR[i]
-            nextR[i] = 0
-    R = nextR
-
+        car.carActionRules()        
+    printRoad(cars)
