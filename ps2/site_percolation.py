00c30b37985d262a0855608a7f3d479e97af7a4a
Jin Sun <jinsun@cs.umd.edu>
2015-11-03 14:07:06 -0500

    modify the code to have 1000 trials

--- ps2/site_percolation.py
+++ ps2/site_percolation.py
@@ -58,100 +58,114 @@ N = 2500
 width = int(math.sqrt(N))
 height = width
 
-#grid, contains cluster label, init to be all empty
-G = [[-1 for i in range(width)] for j in range(height)]
+
+f_lc_vs_p = open("site_percolation_result_lc_vs_p.txt",'w')
+#f.write("Largest component size vs occupation probability\n")
+
+f_p2 = open("site_percolation_result_p2.txt",'a')
+f_p6 = open("site_percolation_result_p6.txt",'a')
+f_p9 = open("site_percolation_result_p9.txt",'a')
+
+for iter in range(1000):
+
+    print("Iter "+str(iter)+"\n")
 
 
-# size array
-G_size = [0]*N
+    #grid, contains cluster label, init to be all empty
+    G = [[-1 for i in range(width)] for j in range(height)]
 
-# need an ordering of filling sites
 
-# random permutation
-siteOrder = range(N)
-random.shuffle(siteOrder)
+    # size array
+    G_size = [0]*N
 
-if verbose:
-    print "Site order is:"
-    print siteOrder
+    # need an ordering of filling sites
 
-# size of largest component vs occupation probability
-lc_vs_p = [0]*N
+    # random permutation
+    siteOrder = range(N)
+    random.shuffle(siteOrder)
 
-# cluster size distribution of different occupation probability
-p02 = G_size[:]
-p06 = G_size[:]
-p09 = G_size[:]
+    if verbose:
+        print "Site order is:"
+        print siteOrder
 
-# adding sites one by one
-for i in range(N):
-    if verbose:    printGrid(G)
-    # get site index
-    siteIdx = siteOrder[i]
-    # convert to sub
-    ri,ci = ind2sub(siteIdx, width)
-    
-    if verbose:     print "Inserting " + str(siteIdx) + " at [" + str(ri) + "," + str(ci) + "]"
-    
-    G[ri][ci] = siteIdx
-    G_size[G[ri][ci]] = 1 # each new site is of its own cluster with size 1
+    # size of largest component vs occupation probability
+    lc_vs_p = [0]*N
 
-    # check if any of four neibhors sites already occupied, if so, add a bond between them. Repeat procedure in bond percolation.
-    neighbors = [[ri-1,ci],[ri+1,ci],[ri,ci-1],[ri,ci+1]]
-    ri_start = ri
-    ci_start = ci
+    # cluster size distribution of different occupation probability
+    p02 = G_size[:]
+    p06 = G_size[:]
+    p09 = G_size[:]
 
-    for ni in range(len(neighbors)):
-        if neighbors[ni][0] >=0 and neighbors[ni][0] <height and neighbors[ni][1] >=0 and neighbors[ni][1] <width:
-            ri_end = neighbors[ni][0]
-            ci_end = neighbors[ni][1]
-            # if bond connects two real clusters
-            if G[ri_start][ci_start] != G[ri_end][ci_end] and G[ri_end][ci_end] != -1:
-                # relabel the smaller cluster
-                if G_size[G[ri_start][ci_start]] < G_size[G[ri_end][ci_end]]:
-                    if verbose:     print "G_size[G["+str(ri_start)+"]["+str(ci_start)+"] < G_size[G["+str(ri_end)+"]["+str(ci_end)+"]]: "+str(G_size[G[ri_start][ci_start]])+" vs "+str(G_size[G[ri_end][ci_end]])
-                    oldLabel = G[ri_start][ci_start]
-                    newLabel = G[ri_end][ci_end]
-                    relabelG(G, ri_start, ci_start, oldLabel, newLabel)
-                else:
-                    if verbose:     print "G_size[G["+str(ri_start)+"]["+str(ci_start)+"] >= G_size[G["+str(ri_end)+"]["+str(ci_end)+"]]: "+str(G_size[G[ri_start][ci_start]])+" vs "+str(G_size[G[ri_end][ci_end]])
-                    oldLabel = G[ri_end][ci_end]
-                    newLabel = G[ri_start][ci_start]
-                    relabelG(G, ri_end, ci_end, oldLabel, newLabel)
-                
-                G_size[newLabel] = G_size[newLabel] + G_size[oldLabel]
-                G_size[oldLabel] = 0
-    lc_vs_p[i] = max(G_size)
+    # adding sites one by one
+    for i in range(N):
+        if verbose:    printGrid(G)
+        # get site index
+        siteIdx = siteOrder[i]
+        # convert to sub
+        ri,ci = ind2sub(siteIdx, width)
+        
+        if verbose:     print "Inserting " + str(siteIdx) + " at [" + str(ri) + "," + str(ci) + "]"
+        
+        G[ri][ci] = siteIdx
+        G_size[G[ri][ci]] = 1 # each new site is of its own cluster with size 1
 
-    if i==int(0.2*N):
-        p02 = G_size[:]
-    if i==int(0.6*N):
-        p06 = G_size[:]
-    if i==int(0.9*N):
-        p09 = G_size[:]
-if verbose:     printGrid(G)
+        # check if any of four neibhors sites already occupied, if so, add a bond between them. Repeat procedure in bond percolation.
+        neighbors = [[ri-1,ci],[ri+1,ci],[ri,ci-1],[ri,ci+1]]
+        ri_start = ri
+        ci_start = ci
 
-f = open("site_percolation_result.txt",'w')
-f.write("Largest component size vs occupation probability\n")
-for item in lc_vs_p:
-    f.write(str(item)+" ")
-f.write("\n")
+        for ni in range(len(neighbors)):
+            if neighbors[ni][0] >=0 and neighbors[ni][0] <height and neighbors[ni][1] >=0 and neighbors[ni][1] <width:
+                ri_end = neighbors[ni][0]
+                ci_end = neighbors[ni][1]
+                # if bond connects two real clusters
+                if G[ri_start][ci_start] != G[ri_end][ci_end] and G[ri_end][ci_end] != -1:
+                    # relabel the smaller cluster
+                    if G_size[G[ri_start][ci_start]] < G_size[G[ri_end][ci_end]]:
+                        if verbose:     print "G_size[G["+str(ri_start)+"]["+str(ci_start)+"] < G_size[G["+str(ri_end)+"]["+str(ci_end)+"]]: "+str(G_size[G[ri_start][ci_start]])+" vs "+str(G_size[G[ri_end][ci_end]])
+                        oldLabel = G[ri_start][ci_start]
+                        newLabel = G[ri_end][ci_end]
+                        relabelG(G, ri_start, ci_start, oldLabel, newLabel)
+                    else:
+                        if verbose:     print "G_size[G["+str(ri_start)+"]["+str(ci_start)+"] >= G_size[G["+str(ri_end)+"]["+str(ci_end)+"]]: "+str(G_size[G[ri_start][ci_start]])+" vs "+str(G_size[G[ri_end][ci_end]])
+                        oldLabel = G[ri_end][ci_end]
+                        newLabel = G[ri_start][ci_start]
+                        relabelG(G, ri_end, ci_end, oldLabel, newLabel)
+                    
+                    G_size[newLabel] = G_size[newLabel] + G_size[oldLabel]
+                    G_size[oldLabel] = 0
+        lc_vs_p[i] = max(G_size)
 
-f.write("p = 0.2, cluster size distribution\n")
-for item in p02:
-    f.write(str(item)+" ")
-f.write("\n")
+        if i==int(0.2*N):
+            p02 = G_size[:]
+        if i==int(0.6*N):
+            p06 = G_size[:]
+        if i==int(0.9*N):
+            p09 = G_size[:]
+    if verbose:     printGrid(G)
 
-f.write("p = 0.6, cluster size distribution\n")
-for item in p06:
-    f.write(str(item)+" ")
-f.write("\n")
+    for item in lc_vs_p:
+        f_lc_vs_p.write(str(item)+" ")
+    f_lc_vs_p.write("\n")
 
-f.write("p = 0.9, cluster size distribution\n")
-for item in p09:
-    f.write(str(item)+" ")
-f.write("\n")
+    #f.write("p = 0.2, cluster size distribution\n")
+    for item in p02:
+        f_p2.write(str(item)+" ")
+    f_p2.write("\n")
 
-f.close()
+    #f.write("p = 0.6, cluster size distribution\n")
+    for item in p06:
+        f_p6.write(str(item)+" ")
+    f_p6.write("\n")
+
+    #f.write("p = 0.9, cluster size distribution\n")
+    for item in p09:
+        f_p9.write(str(item)+" ")
+    f_p9.write("\n")
+
+f_lc_vs_p.close()
+f_p2.close()
+f_p6.close()
+f_p9.close()
 
 print "All done"
