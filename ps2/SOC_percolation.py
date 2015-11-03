67defd884cd88399fcf36616e5a26352ad1925a6
Jin Sun <jinsun@cs.umd.edu>
2015-11-03 17:55:26 -0500

    collect 3(b)(c) data

--- ps2/SOC_percolation.py
+++ ps2/SOC_percolation.py
@@ -46,6 +46,15 @@ def printGrid(G):
             outstr = outstr + Gstr + str(G[i][j]) + " "
         print outstr
 
+# calculate total cluster size
+def sumG_size(G_size):
+    total_size = 0.0
+    for g in G_size:
+        if g != -1:
+            total_size = total_size + G_size[g]
+    return total_size
+
+
 # fill a site with new cluster or spark
 def fillSite(G, G_size, width, siteIdx, fillType):
     # convert to sub
@@ -56,14 +65,14 @@ def fillSite(G, G_size, width, siteIdx, fillType):
         # if already occupied
         if G[ri][ci] != -1:
             if verbose: print "Already occupied, do nothing!"
-            return "filling"
+            return
         G[ri][ci] = t
         G_size[t] = 1 # each new site is of its own cluster with size 1
     else:# filling with spark
         if verbose: print "Spark at [" +str(ri) + "," + str(ci) + "]"
         if G[ri][ci] == -1:
             if verbose: print "But hit an unoccupied site, nothing happend!"
-            return "miss_hit"
+            return "nothing"
         
     
     # check if any of four neibhors sites already occupied, if so, add a bond between them. Repeat procedure in bond percolation.
@@ -117,70 +126,97 @@ N = 2500
 width = int(math.sqrt(N))
 height = width
 
-#grid, contains cluster label, init to be all empty
-G = [[-1 for i in range(width)] for j in range(height)]
+f_density_vs_time = open("SOC_percolation_result_density_vs_time.txt",'w')
+f_cluster_sizes_random = open("SOC_percolation_result_cluster_sizes_random.txt",'w')
+f_cluster_sizes_afterSpark = open("SOC_percolation_result_cluster_sizes_afterSpark.txt",'w')
+f_yield = open("SOC_percolation_result_yield.txt",'w')
+
+for iter in range(1):
+
+    print("Iter "+str(iter)+"\n")
+
+    #grid, contains cluster label, init to be all empty
+    G = [[-1 for i in range(width)] for j in range(height)]
 
 
-# size hash table
-# hash table is used because each time when we add a site, the site label is determined by time step
-G_size = {}
+    # size hash table
+    # hash table is used because each time when we add a site, the site label is determined by time step
+    G_size = {}
 
 
-# occupied density vs time
-d_vs_t = []
+    # occupied density vs time
+    d_vs_t = []
 
-# density vs time after one spark
-d_vs_t_spark = []
-
-# cluster size distribution of different occupation probability
+    # yield, sum of sizes of unburned clusters
+    theyield = []
 
 
-t = 0
-maxT = 20000
-# adding sites by time step
-while t < maxT:
-    
-    sparkValid = False
-    
-    if verbose:    printGrid(G)
-    # randomly pick a site index
-    siteIdx = random.randint(0, N-1)
-    
-    fillSite(G, G_size, width, siteIdx, "normal")
-
-    if t % 100 == 0:
-        # randomly pick a spark index
+    t = 0
+    maxT = 20000
+    transientT = 1000 # when do we start to collect data
+    # adding sites by time step
+    while t < maxT:
+        
+        sparkHit = False
+        
+        if verbose:    printGrid(G)
+        # randomly pick a site index
         siteIdx = random.randint(0, N-1)
-        sparkRes = fillSite(G, G_size, width, siteIdx, "spark")
-        if sparkRes == "hit":
-            sparkValid = True
+        
+        fillSite(G, G_size, width, siteIdx, "normal")
 
+        if t % 100 == 0:
+            # randomly pick a spark index
+            siteIdx = random.randint(0, N-1)
+            G_total_before = sumG_size(G_size)
+            res = fillSite(G, G_size, width, siteIdx, "spark")
+            if res=="hit":
+                sparkHit = True
+            G_total_after = sumG_size(G_size)
+        
+        if t > transientT:
+            # calculate density
+            density = 0.0
+            for g in G_size:
+                if g != -1:
+                    density = density + G_size[g]
+            d_vs_t.append(density/N)
+            if verbose:     print "density: " + str(density/N)
+ 
+            # NOTE:
+            # G_size is a dictionary, iterator will only get the key, not value.
 
-    # calculate density
-    density = 0.0
-    for g in G_size:
-        if g != -1:
-            density = density + G_size[g]
-    d_vs_t.append(density/N)
-    
-    # if just hit a spark
-    if sparkValid:  d_vs_t_spark.append(density/N)
-    if verbose:     print "density: " + str(density/N)
+            # randomly get a snapshot of cluster sizes distribution
+            if random.random() < 0.01:
+                for g in G_size:
+                    if g!= -1:
+                        f_cluster_sizes_random.write(str(G_size[g])+" ")
+                f_cluster_sizes_random.write("\n")
+                       # store cluster sizes after spark hit
+            if sparkHit:
+                for g in G_size:
+                    if g!= -1:
+                        f_cluster_sizes_afterSpark.write(str(G_size[g])+" ")
+                f_cluster_sizes_afterSpark.write("\n")
 
+                # store yield, unburned cluster sizes
+                theyield.append(G_total_before - G_total_after)
+            
+        t = t+1
+    if verbose:     printGrid(G)
 
-    t = t+1
-if verbose:     printGrid(G)
+    for item in d_vs_t:
+        f_density_vs_time.write(str(item)+" ")
+    f_density_vs_time.write("\n")
 
-f = open("SOC_percolation_d_vs_t.txt",'w')
-#f.write("Density vs time\n")
-for item in d_vs_t:
-    f.write(str(item)+" ")
-f.write("\n")
+    for item in theyield:
+        f_yield.write(str(item)+" ")
+    f_yield.write("\n")
 
+f_density_vs_time.close()
+f_cluster_sizes_random.close()
+f_cluster_sizes_afterSpark.close()
+f_yield.close()
 
-f = open("SOC_percolation_d_vs_t_spark.txt",'w')
-for item in d_vs_t_spark:
-    f.write(str(item)+" ")
-f.write("\n")
 
 print "All done"
